2be2f95e3479b21c9332bb0062cd5d5611271fc0
Danis Ziganshin <zigdanis@gmail.com>
2018-07-16 14:30:08 +0300

    Initial Commit

---
+++ SimpleToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleToDo
+//
+//  Created by Danis Ziganshin on 16/07/2018.
+//  Copyright © 2018 ziganshin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
